fd1fa70ed5c2d8ebc8ecf6b799d93b70546ad6fc
Mohammedoumere <oumere09@gmail.com>
2025-04-09 22:30:10 +0300

    Add c++ code of simple calculator

---
+++ simplecalculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std; 
+// Function prototypes 
+int add(int a, int b); 
+int subtract(int a, int b); 
+int multiply(int a, int b); 
+int divide(int a, int b);
+ 
+ int main() {
+ 	int num1 , num2;
+ 	char operation;
+ 	cout<<" Enter two numbers :";
+ 	cin>>num1>>num2;
+ 	cout<<" Choose operation(+, -,*,/):";
+ 	cin>>operation;
+ 	
+ 	return 0;
+ }
